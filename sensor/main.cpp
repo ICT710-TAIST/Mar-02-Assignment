b13ef517580f2ec8ba1da11ef2f863383a37d951
sangrit-siit <Kittikom.San@dome.tu.ac.th>
2020-03-02 23:57:02 +0700

    fix taist-2020-heroku

---
+++ sensor/main.cpp
@@ -0,0 +1,133 @@
+#include <mbed.h>
+
+#include "HTS221Sensor.h"
+
+#define PRODUCTION
+   
+DigitalOut led(LED1);
+InterruptIn button(USER_BUTTON);
+Thread t;
+EventQueue queue(5 * EVENTS_EVENT_SIZE);
+Serial pc(USBTX, USBRX);
+WiFiInterface *wifi;
+
+DevI2C i2c(PB_11, PB_10);
+
+HTS221Sensor hts221(&i2c);
+
+#if defined DEBUG
+
+#include "http_request.h"
+char* endpoint = "http://192.168.2.43:8080/update";
+#else
+#include "https_request.h"
+char* endpoint = "https://taist-2020-heroku.herokuapp.com/update";
+const char SSL_CA_PEM[] ="-----BEGIN CERTIFICATE-----\n"
+"MIIEsTCCA5mgAwIBAgIQBOHnpNxc8vNtwCtCuF0VnzANBgkqhkiG9w0BAQsFADBs\n"
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+"d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j\n"
+"ZSBFViBSb290IENBMB4XDTEzMTAyMjEyMDAwMFoXDTI4MTAyMjEyMDAwMFowcDEL\n"
+"MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3\n"
+"LmRpZ2ljZXJ0LmNvbTEvMC0GA1UEAxMmRGlnaUNlcnQgU0hBMiBIaWdoIEFzc3Vy\n"
+"YW5jZSBTZXJ2ZXIgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC2\n"
+"4C/CJAbIbQRf1+8KZAayfSImZRauQkCbztyfn3YHPsMwVYcZuU+UDlqUH1VWtMIC\n"
+"Kq/QmO4LQNfE0DtyyBSe75CxEamu0si4QzrZCwvV1ZX1QK/IHe1NnF9Xt4ZQaJn1\n"
+"itrSxwUfqJfJ3KSxgoQtxq2lnMcZgqaFD15EWCo3j/018QsIJzJa9buLnqS9UdAn\n"
+"4t07QjOjBSjEuyjMmqwrIw14xnvmXnG3Sj4I+4G3FhahnSMSTeXXkgisdaScus0X\n"
+"sh5ENWV/UyU50RwKmmMbGZJ0aAo3wsJSSMs5WqK24V3B3aAguCGikyZvFEohQcft\n"
+"bZvySC/zA/WiaJJTL17jAgMBAAGjggFJMIIBRTASBgNVHRMBAf8ECDAGAQH/AgEA\n"
+"MA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIw\n"
+"NAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2Vy\n"
+"dC5jb20wSwYDVR0fBEQwQjBAoD6gPIY6aHR0cDovL2NybDQuZGlnaWNlcnQuY29t\n"
+"L0RpZ2lDZXJ0SGlnaEFzc3VyYW5jZUVWUm9vdENBLmNybDA9BgNVHSAENjA0MDIG\n"
+"BFUdIAAwKjAoBggrBgEFBQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQ\n"
+"UzAdBgNVHQ4EFgQUUWj/kK8CB3U8zNllZGKiErhZcjswHwYDVR0jBBgwFoAUsT7D\n"
+"aQP4v0cB1JgmGggC72NkK8MwDQYJKoZIhvcNAQELBQADggEBABiKlYkD5m3fXPwd\n"
+"aOpKj4PWUS+Na0QWnqxj9dJubISZi6qBcYRb7TROsLd5kinMLYBq8I4g4Xmk/gNH\n"
+"E+r1hspZcX30BJZr01lYPf7TMSVcGDiEo+afgv2MW5gxTs14nhr9hctJqvIni5ly\n"
+"/D6q1UEL2tU2ob8cbkdJf17ZSHwD2f2LSaCYJkJA69aSEaRkCldUxPUd1gJea6zu\n"
+"xICaEnL6VpPX/78whQYwvwt/Tv9XBZ0k7YXDK/umdaisLRbvfXknsuvCnQsH6qqF\n"
+"0wGjIChBWUMo0oHjqvbsezt3tkBigAVBRQHvFwY+3sAzm2fTYS5yh+Rp/BIAV0Ae\n"
+"cPUeybQ=\n"
+"-----END CERTIFICATE-----\n";
+ #endif    
+
+float humidity;
+float temperature;
+
+void pressed_handler() {
+
+    pc.printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
+
+    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+        pc.printf("\nConnection error: %d\n", ret);
+        return;
+    }
+
+
+    pc.printf("Success\n\n");
+    printf("MAC: %s\n", wifi->get_mac_address());
+    printf("IP: %s\n", wifi->get_ip_address());
+    printf("Netmask: %s\n", wifi->get_netmask());
+    printf("Gateway: %s\n", wifi->get_gateway());
+    printf("RSSI: %d\n\n", wifi->get_rssi());   
+
+    while(1) {
+
+        hts221.enable();
+        hts221.get_humidity(&humidity);
+        hts221.get_temperature(&temperature);
+        hts221.disable();
+        hts221.reset();
+
+        char body[128];
+        sprintf(body,"{\"humidity\":\%.2f,\"temperature\":\%.2f}", humidity, temperature);
+#if defined DEBUG
+        pc.printf("[DEBUG] ");
+        HttpRequest* request = new HttpRequest(wifi, HTTP_POST, endpoint);
+#else
+        HttpsRequest* request = new HttpsRequest(wifi, SSL_CA_PEM, HTTP_POST, endpoint);
+#endif
+        request->set_header("Content-Type", "application/json");
+        HttpResponse* response = request->send(body, strlen(body));
+        pc.printf("Humidity: %f\t Temperature: %f\t Status: %d\n\r", humidity, temperature, response->get_status_code());
+        //printf("Status is %d - %s\n", response->get_status_code(), response->get_status_message());
+        //printf("body is:\n%s\n", response->get_body());
+        
+        delete request;// also clears out the response
+
+        //wifi->disconnect();
+        //pc.printf("\nDone\n");    
+
+        ThisThread::sleep_for(1000*60);
+   }
+   
+}
+
+int main() {
+
+#if defined DEBUG
+    pc.printf("[DEBUG]\n\r");
+#endif
+
+    wifi = WiFiInterface::get_default_instance();
+    if (!wifi) {
+        printf("ERROR: No WiFiInterface found.\n");
+        return -1;
+    }
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    button.fall(queue.event(pressed_handler));
+    pc.printf("Ready\n");
+
+    if (hts221.init(NULL)!=0) {
+        printf("Could not initializ HTS211\n\r");
+        return -1;
+    }
+
+    while(1) {
+        led = !led;
+        ThisThread::sleep_for(500);
+    }
+
+}
